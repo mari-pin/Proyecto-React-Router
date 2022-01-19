149631e7f5c317b7d7a5e40a9665347bc9d2f66d
Maria Ibañez <mirtraffik@gmail.com>
2022-01-19 21:43:27 +0100

    creacion-hooks, uso en blog.jsx

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Contacto from './routes/Contacto';
 import Blog from './routes/Blog';
 import Inicio from './routes/Inicio'
-import NoEncontardo from './routes/NoEncontardo';
+import NoEncontrado from './routes/NoEncontrado';
 
 
 ReactDOM.render(
@@ -16,10 +16,10 @@ ReactDOM.render(
       <Routes>
         <Route path = '/' element = { <App />}>
           {/* index es la rute del inicio  si falla el path*/}
-        <Route index element = { <Inicio />}/>
+          <Route index element = { <Inicio />}/>
           <Route path = 'blog' element = { <Blog />}/>
           <Route path = 'contacto' element = { <Contacto />}/>
-          <Route path = '*' element = { <NoEncontardo/>}/>
+          <Route path = '*' element = { <NoEncontrado/>}/>
         </Route>
        
       </Routes>
--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useFetch } from '../components/hooks/useFetch';
 
 const Blog = () => {
+    const{data,error,loading} = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+    if (loading){
+        return<h1>Loading....</h1>;
+    }
+    if (error){
+        return<h1>{error}</h1>
+    }
+    
+    
+    
     return (
         <div>
            <h1>Blog</h1> 
+           {data.map((item)=> {
+               <Link to = {`/blog/${item.id}`}>{item.id} - {item.title} </Link>
+           })}
         </div>
     )
 }
